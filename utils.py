d952c799bc765cc37003d58b372c1de80d2995fe
sarahfatima1205 <142889410+sarahfatima1205@users.noreply.github.com>
2024-09-25 21:29:58 +0530

    made some changes in utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ import torch.nn
 
 
 class Visualizer(object):
-    def __init__(self,env="default",**kwargs): #env likely refers to the environment name in Visdom where the visualizations will be stored and organized. If the user does not provide a specific environment when creating a Visualizer object, it will default to "default".
-        #any extra keyword arguments (such as other Visdom configurations) can be passed through **kwargs and will be handled inside the __init__ method.
+    def __init__(self,env="default",**kwargs): 
+        #whatever extra keywords added in the init method of the visdom is passed from here
         self.vis=visdom.Visdom(env=env,**kwargs)
         self.index={}
         
@@ -14,8 +14,8 @@ class Visualizer(object):
         #x=self.index.get(name, 1.00)
         x = self.index.get(name, 0)
         self.vis.line(Y=np.array([y]), X=np.array([x]),
-                      win=str(name), #displays a name on an image
-                      opts=dict(title=name), #opts=dict(title=name): Sets the title of the image window.
+                      win=str(name), #displays a name on image
+                      opts=dict(title=name), 
                       update=None if x==0 else 'append',
                       **kwargs
                       )
@@ -25,7 +25,7 @@ class Visualizer(object):
         self.vis.image(img=img, win=name, opts=dict(title=name))
         
     def lines(self, name, line, X=None):
-        if X is None: #if there is no x axis specifies then it takes index as x
+        if X is None: #if there is no x axis specified then it takes index as x
             self.vis.line(Y=line, win=name)
         else:
             self.vis.line(X=X, Y=line, win=name)
@@ -47,13 +47,13 @@ class Visualizer(object):
         
     
     def minmax_norm(act_map, min_val=None, max_val=None):
-        if min_val is None or max_val is None:
+        if min_val is None or max_val is None: #if min/max value is not specified then it finds it, else it is skipped directly to delta part...
             relu=torch.nn.ReLU()
             max_val=relu(torch.max(act_map, dim=0)[0])
             min_val=relu(torch.min(act_map, dim=0)[0])
         delta=max_val-min_val
         delta[delta<=0]=1
-        ret=(act_map-min_val)/delta
+        ret=(act_map-min_val)/delta #value belongs to range [0,1]
         ret[ret>1]=1
         ret[ret<0]=0
         return ret
